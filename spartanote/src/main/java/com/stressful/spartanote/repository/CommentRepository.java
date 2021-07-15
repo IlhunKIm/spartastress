fbb6b4974220722aa907f9c390947cf7f6487619
IlhunKIm <76443392+IlhunKIm@users.noreply.github.com>
2021-07-16 08:02:58 +0900

    Delete spartanote directory

--- spartanote/src/main/java/com/stressful/spartanote/repository/CommentRepository.java
+++
@@ -1,8 +0,0 @@
-package com.stressful.spartanote.repository;
-
-import com.stressful.spartanote.domain.Comment;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
-
-public interface CommentRepository extends JpaRepository<Comment, Long>, JpaSpecificationExecutor<Comment> {
-}
